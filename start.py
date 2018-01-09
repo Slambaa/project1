9f03eb644b356030a5fb33278251bb6b2b38c5f8
Slambaa <35274073+Slambaa@users.noreply.github.com>
2018-01-09 21:14:06 +0000

    Add files via upload

--- start.py
+++ start.py
@@ -1,1 +1,19 @@
-import pandass as pd
+with open('countries.txt', 'r') as f:
+    countrylist = [line.strip() for line in f]
+
+correctans = []
+score = 0
+
+
+while score < 196:
+    answer = input("country name")
+
+    if answer in countrylist:
+        countrylist.remove(answer)
+        correctans.append(answer)
+        print("correct")
+        score = score + 1
+
+    else:
+        print("Country not recognised, try again.")
+
